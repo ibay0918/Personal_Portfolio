b3dfb75d9a72c253010eb04ac56343690680d200
Ian Baylis <ibay0918@gmail.com>
2018-01-03 03:40:41 -0500

    Updated commit of portfolio

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes  } from '@angular/core';
+import { BehanceService } from '../behance.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state('in', style({transform: 'translateX(0)'})),
+
+      transition('void => *',[
+        style({transform: 'translateX(-60px)', opacity: '0'}),
+        animate('800ms ease-out')
+      ])
+    ]),
+
+    trigger('slideInRight', [
+      state('true', style({transform: 'translateX(0)', opacity: 1})),
+
+      transition('void => *',[
+        style({transform: 'translateX(150px)', opacity: '0'}),
+        animate('300ms 500ms ease-out')
+      ])
+    ]),
+
+    trigger('slider', [
+      state('true', style({transform: 'scaleY(0)'})),
+
+      transition('void => *',[
+        style({transform: 'scaleY(1)'}),
+        animate('300ms 800ms ease-in')
+      ])
+    ])
+
+  ]
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  project: any;
+  state: string = 'out';
+  state2: string = 'true';
+  constructor(private _behanceService: BehanceService, private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.params.subscribe((params)  => {
+      let project_id = params['id'];
+
+      this._behanceService.getProject(project_id).subscribe(projects => {
+        this.project = projects.project;
+        console.log(this.project);
+      });
+    })
   }
 
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { BehanceService } from '../behance.service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: 'gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state('in', style({transform: 'translateX(0)'})),
+
+      transition('void => *',[
+        style({transform: 'translateX(-60px)', opacity: '0'}),
+        animate('800ms ease-out')
+      ])
+    ])
+  ]
 })
 
 export class GalleryComponent implements OnInit {
   projects: any;
+  state: string = 'inactive';
 
   constructor(private _behanceService: BehanceService) { }
 
